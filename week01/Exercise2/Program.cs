314a178ec318f54cfeef645549a82825ab8300ff
jimmy opot alando <jimmyopot18@gmail.com>
2025-03-05 09:07:11 +0300

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        
+        Console.Write("Enter your grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        
+        string letterGrade = "";
+        string gradeSign = "";
+
+        
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        
+        if (letterGrade != "F")
+        {
+            int lastDigit = percentage % 10;
+
+            if (lastDigit >= 7)
+            {
+                gradeSign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                gradeSign = "-";
+            }
+        }
+
+        
+        if (letterGrade == "A" && gradeSign == "+")
+        {
+            gradeSign = "-";  
+        }
+
+        
+        if (letterGrade == "F")
+        {
+            gradeSign = ""; 
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}{gradeSign}");
+
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this time. Better luck next time!");
+        }
     }
 }
