bbcbc99597d276e728e69317d9d5a972d5beafb2
jimmy opot alando <jimmyopot18@gmail.com>
2025-03-05 09:25:19 +0300

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            
+            Console.Write("Enter the magic number (1-100): ");
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            
+            int guess;
+            int numberOfGuesses = 0;
+
+            
+            do
+            {
+                
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                
+                numberOfGuesses++;
+
+                
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {numberOfGuesses} guesses.");
+                }
+            }
+            while (guess != magicNumber); 
+
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+            if (response != "yes")
+            {
+                playAgain = false;
+            }
+        }
+
+        
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
